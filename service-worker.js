1f0c1166f4beeebc1373c3b7d83c6334e619a104
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2023-03-25 11:52:50 -0300

    cache

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (event) => {
     event.waitUntil(
-      caches.open('baby-cache-v1').then((cache) => {
+      caches.open('baby-cache-v2').then((cache) => {
         return cache.addAll([
           '',
           'index.html',
